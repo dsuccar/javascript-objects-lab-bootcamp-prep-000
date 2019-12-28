50550cf758997daaee38bd1a88d94640333db435
Daniel Succar <danielsuccar@icloud.com>
2019-12-28 18:43:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)
 
 
 function deleteFromObjectByKey(obj, key){
-  return Object.assign({}, obj)
+  return Object.assign({}, key)
   delete obj.key;
 return obj
 }
